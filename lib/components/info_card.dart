6700700a164a8a06f9b2006d0a715448e6fd1430
SAOJulius <Julius.krams@gmail.com>
2023-09-18 17:55:33 +0200

    .

--- lib/components/info_card.dart
+++ lib/components/info_card.dart
@@ -1,33 +1,41 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 
-Widget info_card(String title, String info) {
-  return Expanded(
-    child: Container(
-      margin: EdgeInsets.all(26.0),
-      padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 26.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-      ),
-      child: Column(
-        children: [
-          Text(
-            title,
-            style: TextStyle(
-              fontSize: 22.0,
-              fontWeight: FontWeight.bold,
+class InfoCard extends StatelessWidget {
+  final String title;
+  final String info;
+
+  const InfoCard({Key? key, required this.title, required this.info})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.1, right: MediaQuery.of(context).size.width * 0.1),
+        padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 26.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(6.0),
+        ),
+        child: Column(
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          SizedBox(
-            height: 6.0,
-          ),
-          Text(
-            info,
-            style: TextStyle(fontSize: 34.0, fontWeight: FontWeight.bold),
-          ),
-        ],
+            SizedBox(
+              height: 6.0,
+            ),
+            Text(
+              info,
+              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
